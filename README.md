f2570876ee1c640d6fcb947b2daa0ac15959c522
maddhruv <dhruvjainpenny@gmail.com>
2020-01-20 09:48:25 +0530

    add generator

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # npm-statistics
-NPM Download Statistics for ClearTax Open Source Projects
+
+NPM Download Statistics for ClearTax Open Source Projects. Updated Daily.
+
+## Downloads
+
+<!-- Please do not modify this auto generated content -->
+<!-- AUTO-GENERATED-CONTENT:START (PACKAGES) -->
+
+| Name                      | Downloads |
+| ------------------------- | --------- |
+| `post-merge-install`      | 3305      |
+| `@cleartax/zoids`         | 1822      |
+| `pivotal-flow`            | 1572      |
+| `engines-ok`              | 1418      |
+| `@cleartax/eslint-config` | 277       |
+| `props-validator`         | 48        |
+| **Sum**                   | **8442**  |
+
+<!-- AUTO-GENERATED-CONTENT:END -->
+
+### Wanna use `npm-statistics`?
+
+1. `Fork` this repository.
+2. Add your npm username/author in `package.json` as `npm-author` key.
+3. Run `npm start` to generate the Downloads.
---
+++ index.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const _ = require("lodash");
+const fetch = require("node-fetch");
+const table = require("markdown-table");
+const markdownMagic = require("markdown-magic");
+const { "npm-author": author } = require("./package.json");
+
+if (!author) {
+  throw new Error("Please add `npm-author` to your package.json");
+}
+
+(async () => {
+  const date = new Date().toJSON().slice(0, 10),
+    url = `https://npm-stat.com/api/download-counts?author=${author}&from=2019-06-03&until=${date}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  let stats = [];
+
+  for (let [package, downloads] of Object.entries(data)) {
+    stats.push([`\`${package}\``, _.sum(Object.values(downloads))]);
+  }
+
+  const sortedStats = _.reverse(
+    _.sortBy(stats, [
+      function(o) {
+        return o[1];
+      }
+    ])
+  );
+  const sum = _.sumBy(sortedStats, function(o) {
+    return o[1];
+  });
+
+  generate(sortedStats, sum);
+})();
+
+function generate(data, sum) {
+  const config = {
+    transforms: {
+      PACKAGES() {
+        return table([
+          ["Name", "Downloads"],
+          ...data,
+          ["**Sum**", `**${sum}**`]
+        ]);
+      }
+    }
+  };
+
+  markdownMagic(path.join(__dirname, "README.md"), config);
+}
